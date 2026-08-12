9ba5c7b2cc9db070840150cd6e752bc795af1a5e
JbitzISTAKEN <117948725+JbitzISTAKEN@users.noreply.github.com>
2026-08-11 23:44:31 -0400

    Update Starfall.lua

--- Starfall.lua
+++ Starfall.lua
@@ -2,23 +2,24 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local TweenService      = game:GetService("TweenService")
 local Debris            = game:GetService("Debris")
 
-local VFX          = require(ReplicatedStorage.Shared.VFX)
+local VFX             = require(ReplicatedStorage.Shared.VFX)
 local SoundController = require(ReplicatedStorage.Controllers.SoundController)
-local ShakePresets = require(ReplicatedStorage.Shared.ShakePresets)
-local Shake        = require(ReplicatedStorage.Packages.Shake)
+local ShakePresets    = require(ReplicatedStorage.Shared.ShakePresets)
+local Shake           = require(ReplicatedStorage.Packages.Shake)
+local EventController = require(ReplicatedStorage.Controllers.EventController)
 
-local EventScript  = ReplicatedStorage.Controllers.EventController.Events.Starfall
-local Sounds       = ReplicatedStorage.Sounds.Events.Starfall
+local EventScript = ReplicatedStorage.Controllers.EventController.Events.Starfall
+local Sounds      = ReplicatedStorage.Sounds.Events.Starfall
 
 local v4 = Shake.new()
-v4.Amplitude        = 1.5
-v4.Frequency        = 0.1
-v4.FadeInTime       = 0
-v4.FadeOutTime      = 0.6
+v4.Amplitude         = 1.5
+v4.Frequency         = 0.1
+v4.FadeInTime        = 0
+v4.FadeOutTime       = 0.6
 v4.PositionInfluence = Vector3.new(0.2, 0.2, 0.2)
 v4.RotationInfluence = Vector3.new(2.5, 0.5, 0.5)
 
-repeat task.wait() until ReplicatedStorage:GetAttribute("Starfall")
+repeat task.wait() until EventController:GetActiveEventData("Starfall")
 
 local activeMeteors = {}
 local nextStarId    = 1
@@ -50,7 +51,6 @@ local function spawnMeteor(starId, startPos, targetPos, fallDuration)
 
         activeMeteors[starId] = nil
 
-        -- hide meteor
         for _, desc in meteor:GetDescendants() do
             if desc:IsA("ParticleEmitter") then
                 desc.Enabled = false
@@ -60,7 +60,6 @@ local function spawnMeteor(starId, startPos, targetPos, fallDuration)
         end
         Debris:AddItem(meteor, 3)
 
-        -- explosion VFX
         SoundController:PlaySound(Sounds.Impact, targetPos)
 
         local explosion = EventScript.Explosion:Clone()
@@ -76,12 +75,11 @@ local function spawnMeteor(starId, startPos, targetPos, fallDuration)
         end
         Debris:AddItem(explosion, 5)
 
-        -- camera shake if close enough
         local magnitude = (workspace.CurrentCamera.CFrame.Position - targetPos).Magnitude
         if magnitude <= 150 then
             local shakeClone = v4:Clone()
             local scale = math.pow(1 - magnitude / 150, 2)
-            shakeClone.Amplitude        = shakeClone.Amplitude * scale
+            shakeClone.Amplitude         = shakeClone.Amplitude * scale
             shakeClone.RotationInfluence = shakeClone.RotationInfluence * scale
             ShakePresets.BindShakeToCamera(shakeClone)
             shakeClone:Start()
@@ -90,18 +88,18 @@ local function spawnMeteor(starId, startPos, targetPos, fallDuration)
 end
 
 local function loop()
-    while ReplicatedStorage:GetAttribute("Starfall") do
-        local waitTime    = math.random(25) / 100
+    while EventController:GetActiveEventData("Starfall") do
+        local waitTime = math.random(25) / 100
         task.wait(waitTime)
 
-        if not ReplicatedStorage:GetAttribute("Starfall") then break end
+        if not EventController:GetActiveEventData("Starfall") then break end
 
         local mapFloor  = workspace.Events.Starfall.MapFloor
         local floorSize = mapFloor.Size
         local floorCF   = mapFloor.CFrame
 
-        local randomX = math.random(-floorSize.X / 2, floorSize.X / 2)
-        local randomZ = math.random(-floorSize.Z / 2, floorSize.Z / 2)
+        local randomX   = math.random(-floorSize.X / 2, floorSize.X / 2)
+        local randomZ   = math.random(-floorSize.Z / 2, floorSize.Z / 2)
         local targetPos = (floorCF * CFrame.new(randomX, 0, randomZ)).Position
 
         local fixedOrigin = Vector3.new(-1000, 225, 50)
@@ -116,10 +114,9 @@ local function loop()
         spawnMeteor(starId, startPos, targetPos, fallDuration)
     end
 
-    -- cleanup on event end
     for _, data in activeMeteors do
-        if data.Tween  then data.Tween:Cancel() end
-        if data.Model  then data.Model:Destroy() end
+        if data.Tween then data.Tween:Cancel() end
+        if data.Model then data.Model:Destroy() end
     end
     table.clear(activeMeteors)
 end
