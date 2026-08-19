940072ca0452300140c91c0cb9b6f76a581737a2
JbitzISTAKEN <117948725+JbitzISTAKEN@users.noreply.github.com>
2026-08-19 15:23:11 -0400

    Update Bombardiro Crocodilo.lua

--- Bombardiro Crocodilo.lua
+++ Bombardiro Crocodilo.lua
@@ -3,6 +3,8 @@ local CollectionService = game:GetService("CollectionService")
 local HttpService       = game:GetService("HttpService")
 local RunService        = game:GetService("RunService")
 
+if not game:IsLoaded() then game.Loaded:Wait() end
+
 local MathUtils        = require(ReplicatedStorage.Utils.MathUtils)
 local Trove            = require(ReplicatedStorage.Packages.Trove)
 local VFX              = require(ReplicatedStorage.Shared.VFX)
